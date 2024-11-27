1aeeb562a97e22e74981eaa54039fef3b77aff29
yemaneag <abelyemaneab@gmail.com>
2024-11-27 10:41:18 -0500

    declares classes for the problems

--- HackArena/Models/LeetCodeProblem.cs
+++ HackArena/Models/LeetCodeProblem.cs
@@ -8,5 +8,18 @@ namespace HackArena.Models
 {
     public class LeetCodeProblem
     {
+            public string ID { get; set; }
+            public string Title { get; set; } // Problem title
+            public string Description { get; set; } // Problem description
+            public string Solution { get; set; } // Solution code (if needed as a string)
+            public List<TestCase> TestCases { get; set; }
+    }
+    public LeetCodeProblem(int ID, string title, string description,List<TestCase> TestCases, string solution)
+    {
+        ID = ID;
+        Title = title;
+        Description = description;
+        TestCases = TestCases;
+        Solution = solution;
     }
 }
